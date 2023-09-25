9de6857ab8cf7b02e93ad035e2e2835d968588e6
Wisan Dermlim <wsonlines@hotmail.com>
2023-09-25 17:54:42 +0700

    Create addparticipation.sh

---
+++ addparticipation.sh
@@ -0,0 +1,23 @@
+if [ $# -lt 2 ]
+  then
+    echo ""
+    echo "Usage: bash adduser.sh userfile contest_id"
+    echo "example: bash adduser.sh userlist.txt 12"
+    echo ""
+    echo "deail:"
+    echo "userifle : textfile of userformat"
+    echo "contest_id: id of contest"
+
+    echo ""
+    echo "users text format:"
+    echo "format:: username,password,name,surname,email,timezone"
+    echo "example:: wisan01,pass123,Wisan,Dermlim,wisan.d@psu.ac.th,Asia/Bangkok"
+    exit 0
+fi
+
+while IFS= read -r line
+do
+ IFS=','
+ read -ra Arr <<< $line
+ /usr/local/bin/cmsAddParticipation -c $2 ${Arr[0]}
+done < $1
